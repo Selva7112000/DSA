4bed23539f4312f34c0bb7a84cfb8acd0826473a
Selva7112000 <114645891+Selva7112000@users.noreply.github.com>
2025-06-02 22:43:30 +0530

    [1] [Time Beats: 50.75%] [Memory Beats: 10.76%] - LeetPush

--- DSA/Two-Sum.py
+++ DSA/Two-Sum.py
@@ -1,38 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # data = {}
-        # for i in range(len(nums)):
-        #     data[nums[i]] = i
+        pair_idx = {}
 
-        # for i,num in enumerate(nums):
-        #     data1 = target - num
-        #     if data1 in data and data[data1] != i:
-        #         return i,data[data1]
-
-        # for i in range(len(nums)):
-        #     for j in range(i+1,len(nums)):
-        #         if nums[i]+ nums[j] == target:
-        #             return [i,j]
-
-        data = {nums[i]:i for i in range(len(nums))}
-        for i in range(len(nums)):
-            newN = target - nums[i]
-            if newN in data and i != data[newN]:
-                return i,data[newN]
-        # l,r = 0,len(nums) - 1
-        # while l < r:
-        #     if nums[l] + nums[r] == target:
-        #         return [l,r]
-        #     elif nums[l] + nums[r] > target:
-        #         r-=1
-        #     else:
-        #         l+=1
-                
-            
-
-        
-            
-
-
-
-        
+        for i, num in enumerate(nums):
+            if target - num in pair_idx:
+                return [i, pair_idx[target - num]]
+            pair_idx[num] = i
